460bee1d329c795dc8d0106a0432a9237e1ef808
misolab <do.ockhyun@gmail.com>
2021-02-14 13:00:19 +0900

    review - list ui

--- site/src/main/java/com/misolab/site/controller/IndexController.java
+++ site/src/main/java/com/misolab/site/controller/IndexController.java
@@ -1,21 +1,62 @@
 package com.misolab.site.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import lombok.Builder;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-    //  리스트
+    @Getter
+    @Builder
+    static class Review {
+        int id;
+        String title;
+        String text;
+        String updated;
+        String book;
+        int bookId;
+        String favorite;
+    }
+
+    @ModelAttribute
+    public void common(Model model) {
+
+        List<Review> popular = IntStream.range(1, 3)
+                                .mapToObj(i -> Review.builder()
+                                .id(i)
+                                .title("Even the all-powerful Pointing has no control")
+                                .updated("June 28, 2019")
+                                .build())
+                                .collect(Collectors.toList());
+        model.addAttribute("popular", popular);
+    }
+
+    // 리스트
     @GetMapping({"/", "/review"})
     public String index(Model model) {
+        List<Review> list = IntStream.range(1, 50)
+        .mapToObj(i -> Review.builder()
+        .id(i)
+        .title("독후감 제목")
+        .text("독후감 주저리~ 독후감 주저리~ 독후감 주저리~ 독후감 주저리~ 독후감 주저리~ 독후감 주저리~ 독후감 주저리~ 독후감 주저리~ 독후감 주저리~ 독후감 주저리~ 독후감 주저리~ 독후감 주저리~ 독후감 주저리~ 독후감 주저리~ 독후감 주저리~ 독후감 주저리~ ")
+        .book("책제목 / 출판사")
+        .bookId(100)
+        .updated("June 28, 2019")
+        .build())
+        .collect(Collectors.toList());
+
+        model.addAttribute("list", list);
         return "index";
     }
 
